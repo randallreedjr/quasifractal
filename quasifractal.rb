72407dbbe59806c698ee346d5aff2457dafd6736
Randall Reed, Jr <randallreedjr@gmail.com>
2019-04-14 17:24:48 -0400

    Generate html

--- quasifractal.rb
+++ quasifractal.rb
@@ -18,6 +18,16 @@ class Quasifractal
     end
   end
 
+  def first_move!
+    # should this mutate?
+    @board = empty_board
+    @board.map!.with_index do |new_board, index|
+      new_board = @board.dup
+      new_board[index] = 'X'
+      new_board
+    end
+  end
+
   def second_move
     @board = first_move
     @board.map do |first_move_board|
@@ -32,12 +42,177 @@ class Quasifractal
     end
   end
 
-  def nth_move(n)
-    return empty_board if n == 0
+  def second_move!
+    first_move!
+    @board.map! do |first_move_board|
+      first_move_board.map.with_index do |new_board, index|
+        # Skip if a move has already been made
+        if new_board.nil?
+          new_board = first_move_board.dup
+          new_board[index] = 'O'
+        end
+        new_board
+      end
+    end
+  end
+
+  def third_move!
+    second_move!
+    @board.map! do |first_move_board|
+      first_move_board.map.with_index do |second_move_board|
+        next unless second_move_board.is_a? Array
+        second_move_board.map.with_index do |new_board, index|
+          if new_board.nil?
+            new_board = second_move_board.dup
+            new_board[index] = 'X'
+          end
+          new_board
+        end
+      end
+    end
+  end
+
+  def nth_move!(n, board = empty_board)
+    return board if n == 0
+    return board unless board.is_a? Array
     # What next? How do we get to the bottom of an arbitratily deeply nested array
     # What if we convert to a hash, where the key is the level of nesting?
     # Maybe an array isn't the right approach?
 
     # Depth first search?
+    # How to determine depth
+    #make a move
+    old_board = board.dup
+    # binding.pry if board == 'X'
+    board.map!.with_index do |new_board, index|
+      if new_board.nil?
+        new_board = old_board.dup
+        # new_board[index] = mark(old_board)
+        # need to adjust index based on position of existing move
+        position = index
+        until new_board[index].nil?
+          position += 1
+        end
+        new_board[position] = mark(old_board)
+      end
+      new_board
+    end
+    board.map! do |new_board|
+      # binding.pry
+      nth_move!(n - 1, new_board)
+    end
+    @board = board
+    return board
+  end
+
+  def depth
+    level_count = 0
+    current_level = board
+    while current_level.any? {|el| el.is_a? Array}
+      current_level = current_level.detect {|el| el.is_a? Array}
+      level_count += 1
+    end
+    level_count
+  end
+
+  def mark(board)
+    board.count(nil).odd? ? 'X' : 'O'
+    # depth.even? ? 'X' : 'O'
+  end
+
+  def to_html
+    puts "<html>"
+    puts "<head>#{style}</head>"
+    puts "<body>"
+    # puts "<table>"
+    array_to_html(board, 9)
+    # puts "</table>"
+    puts "</body></html>"
+  end
+
+  def array_to_html(array, border_width)
+    puts "<table>"
+
+    array.each.with_index do |el, index|
+      if index % 3 == 0
+        puts "<tr>"
+      end
+
+      border_styles = []
+      if index % 3 == 0
+        border_styles << "left-column-#{border_width}"
+      elsif index % 3 == 2
+        border_styles << "right-column-#{border_width}"
+      end
+
+      if index / 3 == 0
+        border_styles << "top-row-#{border_width}"
+      elsif index / 3 == 2
+        border_styles << "bottom-row-#{border_width}"
+      end
+
+      puts "<td class=\"#{border_styles.join(' ')}\">"
+
+      if el.is_a? Array
+        array_to_html(el, border_width - 1)
+      elsif el.nil?
+        puts "&nbsp;"
+      else
+        puts "#{el}"
+      end
+
+      puts "</td>"
+
+      if index % 3 == 2
+        puts "</tr>"
+      end
+    end
+
+    puts "</table>"
+  end
+
+  def style
+    style = "<style>\n"
+    style += "td { text-align: center; }\n"
+    style += (1..9).flat_map do |size|
+      [
+        ".top-row-#{size} { border-bottom: #{size}px solid black; padding-bottom: #{size}px; }",
+        ".bottom-row-#{size} { border-top: #{size}px solid black; padding-top: #{size}px; }",
+        ".left-column-#{size} { border-right: #{size}px solid black; padding-right: #{size}px; }",
+        ".right-column-#{size} { border-left: #{size}px solid black; padding-left: #{size}px; }"
+      ]
+    end.join("\n")
+    style += "\n</style>"
+    return style
+    # style = <<~STYLE
+    #   <style>
+    #     td { text-align: center; }
+    #     .top-row-1 { border-bottom: 1px solid black; padding-bottom: 1px; }
+    #     .bottom-row-1 { border-top: 1px solid black; padding-top: 1px; }
+    #     .left-column-1 { border-right: 1px solid black; padding-right: 1px; }
+    #     .right-column-1 { border-left: 1px solid black; padding-left: 1px; }
+
+    #     .top-row-1 { border-bottom: 1px solid black; padding-bottom: 1px; }
+    #     .bottom-row-1 { border-top: 1px solid black; padding-top: 1px; }
+    #     .left-column-1 { border-right: 1px solid black; padding-right: 1px; }
+    #     .right-column-1 { border-left: 1px solid black; padding-left: 1px; }
+
+    #     .top-row-7 { border-bottom: 7px solid black; padding-bottom: 7px; }
+    #     .bottom-row-7 { border-top: 7px solid black; padding-top: 7px; }
+    #     .left-column-7 { border-right: 7px solid black; padding-right: 7px; }
+    #     .right-column-7 { border-left: 7px solid black; padding-left: 7px; }
+
+    #     .top-row-8 { border-bottom: 8px solid black; padding-bottom: 8px; }
+    #     .bottom-row-8 { border-top: 8px solid black; padding-top: 8px; }
+    #     .left-column-8 { border-right: 8px solid black; padding-right: 8px; }
+    #     .right-column-8 { border-left: 8px solid black; padding-left: 8px; }
+
+    #     .top-row-9 { border-bottom: 9px solid black; padding-bottom: 9px; }
+    #     .bottom-row-9 { border-top: 9px solid black; padding-top: 9px; }
+    #     .left-column-9 { border-right: 9px solid black; padding-right: 9px; }
+    #     .right-column-9 { border-left: 9px solid black; padding-left: 9px; }
+    #   </style>
+    # STYLE
+    # style
   end
 end
--- spec/quasifractal_spec.rb
+++ spec/quasifractal_spec.rb
@@ -19,7 +19,7 @@ describe Quasifractal do
     end
   end
 
-  describe '#first_move' do
+  fdescribe '#first_move' do
     it 'returns a nested array' do
       board = Quasifractal.new.first_move
 
@@ -53,6 +53,48 @@ describe Quasifractal do
       expect(board[6][6]).to eq 'X'
       expect(board[7][7]).to eq 'X'
       expect(board[8][8]).to eq 'X'
+    end
+  end
+
+  xdescribe '#third_move' do
+    it 'returns a partially nested array' do
+      board = Quasifractal.new.third_move!
+      binding.pry
+
+      expect(board.is_a?(Array)).to be true
+      expect(board[0].is_a?(Array)).to be true
+      expect(board[0][0].is_a?(Array)).to be false
+      expect(board[0][1].is_a?(Array)).to be true
+      expect(board[0][1][0].is_a?(Array)).to be false
+    end
+
+    it 'returns an array containing 9 arrays' do
+      board = Quasifractal.new.second_move
+
+      expect(board.length).to eq 9
+      expect(board.compact.length).to eq 9
+    end
+
+    it 'returns an array with 72 moves made' do
+      board = Quasifractal.new.second_move
+
+      # 9 + 9*(9*8) => 9 first moves, 72 second moves for each first moves
+      expect(board.flatten.length).to be 657
+      # 9 + 2*72 => 9 first moves (1 char), 72 second moves (2 char)
+      expect(board.flatten.compact.length).to be 153
+    end
+
+    it 'returns an array with the second moves made' do
+      board = Quasifractal.new.second_move
+
+      expect(board[0][1][1]).to eq 'O'
+      expect(board[0][2][2]).to eq 'O'
+      expect(board[0][3][3]).to eq 'O'
+      expect(board[0][4][4]).to eq 'O'
+      expect(board[0][5][5]).to eq 'O'
+      expect(board[0][6][6]).to eq 'O'
+      expect(board[0][7][7]).to eq 'O'
+      expect(board[0][8][8]).to eq 'O'
     end
   end
 
@@ -96,7 +138,7 @@ describe Quasifractal do
     end
   end
 
-  fdescribe 'nth move' do
+  describe 'nth move' do
     context 'when n is 0' do
       it 'returns an empty board' do
         board = Quasifractal.new.nth_move(0)
@@ -104,6 +146,81 @@ describe Quasifractal do
 
         expect(board).to eq empty_board
       end
+    end
+
+    context 'when n is 1' do
+      xit 'returns a board with the first X move' do
+        quasifractal = Quasifractal.new
+        board = quasifractal.nth_move(1)
+        first_move = Quasifractal.new.first_move
+
+        expect(board).to eq first_move
+      end
+    end
+
+    context 'when n is 2' do
+      it 'returns a board with thefirst O move' do
+        quasifractal = Quasifractal.new
+        board = quasifractal.nth_move(2)
+        second_move = Quasifractal.new.second_move
+
+        expect(board).to eq second_move
+      end
+    end
+
+    context 'when n is 3' do
+      it 'returns a board with the second X move' do
+        quasifractal = Quasifractal.new
+        board = quasifractal.nth_move(3)
+        # second_move = Quasifractal.new.second_move
+
+        # expect(board).to eq second_move
+        binding.pry
+      end
+    end
+  end
+
+  describe 'depth' do
+    context 'empty board' do
+      it 'returns 0' do
+        quasifractal = Quasifractal.new
+
+        expect(quasifractal.depth).to eq 0
+      end
+    end
+
+    context 'first move' do
+      it 'returns 1' do
+        quasifractal = Quasifractal.new
+        quasifractal.first_move!
+
+        expect(quasifractal.depth).to eq 1
+      end
+    end
+
+    context 'second move' do
+      it 'returns 2' do
+        quasifractal = Quasifractal.new
+        quasifractal.second_move!
+
+        expect(quasifractal.depth).to eq 2
+      end
+    end
+  end
+
+  describe 'to_html' do
+    it 'prints a first-move board' do
+      quasifractal = Quasifractal.new
+      quasifractal.nth_move!(1)
+
+      quasifractal.to_html
+    end
+
+    fit 'prints a second-move board' do
+      quasifractal = Quasifractal.new
+      quasifractal.nth_move!(2)
+
+      quasifractal.to_html
     end
   end
 end
